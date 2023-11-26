a4129da5164a379fdae4fd78365a469b3f19b5f3
Wilson Castillo <115800617+Wilson8jsn@users.noreply.github.com>
2023-11-26 13:07:36 -0500

    Initial commit

---
+++ src/main/kotlin/com/UV_Shield/Factura_nativas/FacturaNativasApplication.kt
@@ -0,0 +1,11 @@
+package com.UV_Shield.Factura_nativas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FacturaNativasApplication
+
+fun main(args: Array<String>) {
+	runApplication<FacturaNativasApplication>(*args)
+}
---
+++ src/test/kotlin/com/UV_Shield/Factura_nativas/FacturaNativasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.UV_Shield.Factura_nativas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FacturaNativasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
