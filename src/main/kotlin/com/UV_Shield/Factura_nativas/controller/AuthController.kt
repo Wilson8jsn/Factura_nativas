b541a18917ff6cb9640d47a1f7dc3b934f4aa1df
Wilson Castillo <dc105677543@gmail.com>
2024-01-14 22:51:31 -0500

    commit

--- src/main/kotlin/com/UV_Shield/Factura_nativas/controller/AuthController.kt
+++ src/main/kotlin/com/UV_Shield/Factura_nativas/controller/AuthController.kt
@@ -8,8 +8,7 @@ import com.UV_Shield.Factura_nativas.utils.JwtUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.authentication.*
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -25,10 +24,25 @@ class AuthController {
     private val jwtUtil: JwtUtil? = null
 
     @PostMapping("/login")
-    fun login(@RequestBody loginDto: LoginDto): ResponseEntity<*>? {
-        val login = UsernamePasswordAuthenticationToken(loginDto.username, loginDto.password)
-        val authentication: Authentication = authenticationManager!!.authenticate(login)
-        val response = TokenDto().apply { jwt= jwtUtil!!.create(loginDto.username)}
-        return ResponseEntity(response, HttpStatus.OK)
+    fun login(@RequestBody loginDto: LoginDto): ResponseEntity<*> {
+        try {
+            val login = UsernamePasswordAuthenticationToken(loginDto.username, loginDto.password)
+            val authentication: Authentication = authenticationManager!!.authenticate(login)
+            val response = TokenDto().apply { jwt = jwtUtil!!.create(loginDto.username) }
+            return ResponseEntity(response, HttpStatus.OK)
+        } catch (ex: BadCredentialsException) {
+            // Credenciales incorrectas
+            return ResponseEntity("Invalid username or password", HttpStatus.UNAUTHORIZED)
+        } catch (ex: LockedException) {
+            // Cuenta bloqueada
+            return ResponseEntity("Account locked", HttpStatus.UNAUTHORIZED)
+        } catch (ex: DisabledException) {
+            // Cuenta deshabilitada
+            return ResponseEntity("Account disabled", HttpStatus.UNAUTHORIZED)
+        } catch (ex: Exception) {
+            // Otro error durante la autenticación
+            return ResponseEntity("Authentication error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
+
 }
